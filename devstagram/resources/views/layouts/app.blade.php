4d90672fb6461a9f64082b9c33fe25e74aad0e16
cuauhvip07 <cuauvillalba1@gmail.com>
2024-03-23 10:13:51 -0600

    Instalacion tailwindcss
    Instalacion de tailwindcss

--- devstagram/resources/views/layouts/app.blade.php
+++ devstagram/resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        @vite('resources/css/app.css')
         <title>Laravel - @yield('titulo')</title>
 
         
@@ -13,6 +13,6 @@
             <a href="/">Inicio</a>
             <a href="/informacion">Informacion</a>
         </nav>
-        <h1>@yield('body')</h1>
+        <h1 class=" text-red-400 text-4xl">@yield('body')</h1>
     </body>
 </html>
