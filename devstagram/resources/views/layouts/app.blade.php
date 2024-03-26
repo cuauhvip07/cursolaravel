a31b22407dc3be80a46c53163c0fff87ef6e48a8
cuauhvip07 <cuauvillalba1@gmail.com>
2024-03-25 21:29:26 -0600

    Routes
    Se implementan las routes en caso de querer cambiar la url y se implementa la accion de seguridad de los formularios

--- devstagram/resources/views/layouts/app.blade.php
+++ devstagram/resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
             <h1 class=" text-3xl font-black">DevStagram</h1>
             <nav class=" flex gap-2 items-center">
                 <a class=" font-bold uppercase text-gray-600" href="#">Login</a>
-                <a class=" font-bold uppercase text-gray-600" href="/crear-cuenta">Crear Cuenta</a>
+                <a class=" font-bold uppercase text-gray-600" href="{{ route('register') }}">Crear Cuenta</a>
             </nav>
         </div> 
        </header>
