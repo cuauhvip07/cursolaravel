6f39ef93b302046165652506541bfe532758d2f1
cuauhvip07 <cuauvillalba1@gmail.com>
2024-04-17 21:06:21 -0600

    Cambio idioma y alertas
    Se cambia el idioma a español y se agrega los estilos a las alertas

--- devjobs/tailwind.config.js
+++ devjobs/tailwind.config.js
@@ -9,13 +9,7 @@ export default {
         './resources/views/**/*.blade.php',
     ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
+
 
     plugins: [forms],
 };
