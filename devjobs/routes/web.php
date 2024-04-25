0d7d0a08d1c4bcc524f0a884e172e4a31d637166
cuauhvip07 <cuauvillalba1@gmail.com>
2024-04-24 20:35:21 -0600

    Vacantes
    Se hacen las migraciones, controladores y modelos para las vacantes

--- devjobs/routes/web.php
+++ devjobs/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\VacanteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,9 +11,7 @@ Route::get('/', function () {
 // auth -> Tiene que estar autenticado . Verified -> Tiene que estar verificado
 // En el modelo de user se debe de agregar un implements. en el .env se ve como acceder al mail local
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [VacanteController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
