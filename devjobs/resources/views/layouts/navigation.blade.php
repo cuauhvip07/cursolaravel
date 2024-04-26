7f145665bc492cf2586442f0c5c22e48911f02a4
cuauhvip07 <cuauvillalba1@gmail.com>
2024-04-25 20:01:00 -0600

    Instalando livewire
    Se instala livewire para su uso

--- devjobs/resources/views/layouts/navigation.blade.php
+++ devjobs/resources/views/layouts/navigation.blade.php
@@ -17,8 +17,8 @@
                         {{ __('Mis Vacantes') }}
                     </x-nav-link>
 
-                    <x-nav-link :href="route('vacantes.index')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('vacantes.create')" :active="request()->routeIs('vacantes.create')">
+                        {{ __('Crear Vacante') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -75,6 +75,10 @@
             <x-responsive-nav-link :href="route('vacantes.index')" :active="request()->routeIs('vacantes.index')">
                 {{ __('Mis Vacantes') }}
             </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('vacantes.create')" :active="request()->routeIs('vacantes.create')">
+                {{ __('Crear Vacante') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
@@ -96,7 +100,7 @@
                     <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
-                        {{ __('Log Out') }}
+                        {{ __('Cerrar Sesión') }}
                     </x-responsive-nav-link>
                 </form>
             </div>
--- devjobs/routes/web.php
+++ devjobs/routes/web.php
@@ -12,6 +12,7 @@ Route::get('/', function () {
 // En el modelo de user se debe de agregar un implements. en el .env se ve como acceder al mail local
 
 Route::get('/dashboard', [VacanteController::class, 'index'])->middleware(['auth', 'verified'])->name('vacantes.index');
+Route::get('/vacantes/create', [VacanteController::class, 'create'])->middleware(['auth', 'verified'])->name('vacantes.create');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
