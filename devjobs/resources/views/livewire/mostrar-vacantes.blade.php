76a2d0e23dc748e34860daec353c664fee80a0b1
cuauhvip07 <cuauvillalba1@gmail.com>
2024-04-27 09:51:33 -0600

    Listado vacantes
    Se ve una funcion de forelse de laravel que nos ayuda a validar si hay vacantes o no

--- devjobs/resources/views/livewire/mostrar-vacantes.blade.php
+++ devjobs/resources/views/livewire/mostrar-vacantes.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
 
-    @foreach ($vacantes as $vacante )
+    @forelse ( $vacantes as $vacante )
         <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">
             <div class=" space-y-3">
                 <a href="#" class="text-xl font-bold">{{$vacante->titulo}}</a>
@@ -14,7 +14,10 @@
                 <a href="#" class=" bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center ">Eliminar</a>
             </div>
         </div>    
-    @endforeach
+    @empty
 
+        <p class=" p-3 text-center text-sm text-gray-600">No hay vacantes</p>
+        
+    @endforelse
 
 </div>
