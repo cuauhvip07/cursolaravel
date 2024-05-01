823e056f007ca014720a55a36a71f921a7460c3a
cuauhvip07 <cuauvillalba1@gmail.com>
2024-04-30 18:51:21 -0600

    Mostrando información de la vacante
    Se muestra la información de la vacante en la vista con livewire

--- devjobs/resources/views/vacantes/show.blade.php
+++ devjobs/resources/views/vacantes/show.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 my-5">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-3">
                 
-             
+             <livewire:mostrar-vacante :vacante="$vacante"/>
 
             </div>
         </div>
