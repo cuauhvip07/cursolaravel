083913a36db4902eeabe1e84326b2ff05bc0f3a5
cuauhvip07 <cuauvillalba1@gmail.com>
2024-04-30 19:32:10 -0600

    Crear cuenta para invitados
    En caso de que el usuario no tenga cuenta y desee postularse a una vacante, tendrá que crear una cuenta

--- devjobs/resources/views/livewire/mostrar-vacante.blade.php
+++ devjobs/resources/views/livewire/mostrar-vacante.blade.php
@@ -15,13 +15,24 @@
    </div>
 
    <div class="md:grid md:grid-cols-6 gap-4">
-    <div class="md:col-span-2">
-        <img src="{{asset('storage/vacantes/'.$vacante->imagen)}}" alt="{{'Imagen vacante' . $vacante->titulo}}">
-    </div>
+        <div class="md:col-span-2">
+            <img src="{{asset('storage/vacantes/'.$vacante->imagen)}}" alt="{{'Imagen vacante' . $vacante->titulo}}">
+        </div>
 
-    <div class="md:col-span-4">
-        <h2 class="text-2xl font-bold mb-5">Descripción del Puesto</h2>
-        <p>{{$vacante->descripcion}}</p>
-    </div>
+        <div class="md:col-span-4">
+            <h2 class="text-2xl font-bold mb-5">Descripción del Puesto</h2>
+            <p>{{$vacante->descripcion}}</p>
+        </div>
    </div>
+
+   @guest
+        <div class="mt-5 bg-gray-50 border border-dashed p-5 text-center">
+            <p>
+                ¿Deseas postularte a esta vacante?
+                <a href="{{route('register')}}" class="font-bold text-indigo-600">Obten una cuenta y aplica a esta y otras vacantes</a>
+            </p>
+        </div>
+   @endguest
+
+   
 </div>
