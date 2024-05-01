746bbeeb6789f28c893227ba0431d659078582dc
cuauhvip07 <cuauvillalba1@gmail.com>
2024-04-30 19:11:09 -0600

    Img y descripcion vacante
    Se muestra la imagen y descripción de la vacante

--- devjobs/resources/views/livewire/mostrar-vacante.blade.php
+++ devjobs/resources/views/livewire/mostrar-vacante.blade.php
@@ -13,4 +13,15 @@
             <p class="font-bold text-sm uppercase text-gray-800 my-3">Salario: <span class="normal-case font-normal">{{$vacante->salario->salario}} </span> </p>
         </div>
    </div>
+
+   <div class="md:grid md:grid-cols-6 gap-4">
+    <div class="md:col-span-2">
+        <img src="{{asset('storage/vacantes/'.$vacante->imagen)}}" alt="{{'Imagen vacante' . $vacante->titulo}}">
+    </div>
+
+    <div class="md:col-span-4">
+        <h2 class="text-2xl font-bold mb-5">Descripción del Puesto</h2>
+        <p>{{$vacante->descripcion}}</p>
+    </div>
+   </div>
 </div>
