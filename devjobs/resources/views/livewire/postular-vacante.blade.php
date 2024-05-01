3228a9caadcbfada1943b079734b0d4d3dd1edc9
cuauhvip07 <cuauvillalba1@gmail.com>
2024-05-01 17:09:26 -0600

    Candidatos
    Se crean los componentes para las postulaciones de los candidatos

--- devjobs/resources/views/livewire/postular-vacante.blade.php
+++ devjobs/resources/views/livewire/postular-vacante.blade.php
@@ -1,3 +1,21 @@
-<div>
- <p>Aqui se mostrara el formulario</p>
+<div class="bg-gray-100 p-5 mt-10 flex flex-col justify-center items-center">
+    <h3 class="text-center text-2xl font-bold my-4">Postularme a esta Vacante</h3>
+
+    <form action="w-96 mt-5">
+        <div class="mb-4">
+            <x-input-label 
+            for="cv" 
+            :value="__('Curriculum (PDF)')" />
+
+            <x-text-input 
+            id="cv" 
+            class="block mt-1 w-full" 
+            type="file" 
+            accept=".pdf"/>
+        </div>
+
+        <x-primary-button class="my-5">
+            Postularme
+        </x-primary-button>
+    </form>
 </div>
