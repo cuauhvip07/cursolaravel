560e7883d51e3f910cfe350fbad05a024bcf0970
cuauhvip07 <cuauvillalba1@gmail.com>
2024-05-01 19:55:24 -0600

    Creación notificacion
    Se crea la notificación y su migración

---
+++ devjobs/app/Notifications/NuevoCandidato.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NuevoCandidato extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     * Aqui vas a pasar la informacion que quieras que tenga la notificacion
+     */
+
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     * 
+     * Informacion que va a ser enviada en un email
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    // Alamcena las notificaciones en la bd
+    public function toDatabase(object $notifiable)
+    {
+
+    }
+}
