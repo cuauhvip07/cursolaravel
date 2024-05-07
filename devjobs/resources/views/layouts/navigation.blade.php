75b9d91541dd2da142dd6fa6d32adfa53d47d322
cuauhvip07 <cuauvillalba1@gmail.com>
2024-05-06 19:27:55 -0600

    Editar navegacion
    Se edita la navegación, para que un desarrollador no vea las vacantes y crear vacantes

--- devjobs/resources/views/layouts/navigation.blade.php
+++ devjobs/resources/views/layouts/navigation.blade.php
@@ -11,17 +11,20 @@
                 </div>
 
                 @auth
-                    <!-- Navigation Links -->
-                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
 
-                        <x-nav-link :href="route('vacantes.index')" :active="request()->routeIs('vacantes.index')">
-                            {{ __('Mis Vacantes') }}
-                        </x-nav-link>
+                    @can('create',App\Models\Vacante::class)
+                        <!-- Navigation Links -->
+                        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
 
-                        <x-nav-link :href="route('vacantes.create')" :active="request()->routeIs('vacantes.create')">
-                            {{ __('Crear Vacante') }}
-                        </x-nav-link>
-                    </div>
+                            <x-nav-link :href="route('vacantes.index')" :active="request()->routeIs('vacantes.index')">
+                                {{ __('Mis Vacantes') }}
+                            </x-nav-link>
+
+                            <x-nav-link :href="route('vacantes.create')" :active="request()->routeIs('vacantes.create')">
+                                {{ __('Crear Vacante') }}
+                            </x-nav-link>
+                        </div>
+                    @endcan
                 @endauth
 
                 
@@ -33,11 +36,11 @@
 
                 @auth
                     {{-- notificaciones  --}}
-                    @if (auth()->user()->rol === 2)
+                    @can('create',App\Models\Vacante::class)
                         <a href="{{route('notificaciones')}}" class="mr-2 w-7 h-7 bg-indigo-600 hover:bg-indigo-700 rounded-full flex flex-col justify-center items-center text-sm font-extrabold text-white">
                             {{auth()->user()->unreadNotifications->count()}}
                         </a>
-                    @endif
+                    @endcan
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
