be771a8307932f1a852c8b202f28f917effa075c
cuauhvip07 <cuauvillalba1@gmail.com>
2024-05-10 12:07:02 -0600

    Filtrando vacantes
    Se filtran las vacantes por su nombre

--- devjobs/app/Livewire/HomeVacantes.php
+++ devjobs/app/Livewire/HomeVacantes.php
@@ -4,9 +4,11 @@ namespace App\Livewire;
 
 use App\Models\Vacante;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class HomeVacantes extends Component
 {
+    use WithPagination;
 
     public $termino;
     public $salario;
@@ -14,7 +16,7 @@ class HomeVacantes extends Component
 
     // Poner los elementos que se van a escuchar por un evento, el evento tambien se cuentra en FiltrarVacante
     // Cuando ocurre el evento de terminos busqueda manda a llamr la funcion de buscar 
-    protected $listenrs = ['terminosBusqueda' => 'buscar'];
+    protected $listeners = ['terminosBusqueda' => 'buscar'];
 
     public function buscar($termino,$categoria,$salario)
     {
@@ -27,8 +29,15 @@ class HomeVacantes extends Component
     public function render()
     {
         
-        $vacantes = Vacante::all();
+        // $vacantes = Vacante::all();
         
+        // En laravel si utilizas where es por que un valor SI existe
+        // When se ejecuta solamente en EN CASO DE QUE EXISTA
+
+        $vacantes = Vacante::when($this->termino , function($query){
+            $query->where('titulo','LIKE', "%" . $this->termino . "%");
+        })->paginate(20);
+
         return view('livewire.home-vacantes',[
             'vacantes'=>$vacantes,
         ]);
