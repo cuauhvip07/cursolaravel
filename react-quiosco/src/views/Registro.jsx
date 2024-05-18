54b535242c417ed2a880e12a35bc304961bac605
cuauhvip07 <cuauvillalba1@gmail.com>
2024-05-17 22:06:35 -0600

    Inicio formulario registro
    Se empiezan a crear las etiquetas para el formulario del registro

--- react-quiosco/src/views/Registro.jsx
+++ react-quiosco/src/views/Registro.jsx
@@ -3,8 +3,31 @@
 
 export default function Registro() {
   return (
-    <div>
-        Registro
-    </div>
+    <>
+      <h1 className=" text-4xl font-black">Crea tu Cuenta</h1>
+      <p>Crea tu cuenta llenando el formulario</p>
+
+      <div  className=" bg-white shadow-md rounded-md mt-10 px-5 py-10">
+        <form>
+            <div className=" mb-4">
+                <label 
+                    className=" text-slate-800"
+                    htmlFor="name">
+
+                    Nombre:
+                </label>
+
+                <input 
+                    type="text" 
+                    id="name"
+                    className=" mt-2 block p-3 bg-gray-50 w-full"
+                    name="name"
+                    placeholder="Tu nombre"
+                />
+
+            </div>
+        </form>
+      </div>
+    </>
   )
 }
