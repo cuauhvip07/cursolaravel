e2ea9d2c110c687ff7d62c3fe40b3eb8a3b04d52
cuauhvip07 <cuauvillalba1@gmail.com>
2024-05-20 20:23:44 -0600

    Mostrando informacion
    Se muestra la imagen, precio y nombre del producto

--- react-quiosco/src/layouts/components/Producto.jsx
+++ react-quiosco/src/layouts/components/Producto.jsx
@@ -5,8 +5,17 @@ export default function Producto({producto}) {
     const {nombre,imagen,precio} = producto
   return (
    
-    <div>
-      Productos
+    <div className=" border p-3 shadow bg-white">
+        <img 
+            src={`/img/${imagen}.jpg`} 
+            alt={`Imagen ${nombre}`}
+            className=" w-full"
+        />
+        <div className=" p-5">
+            <h3 className=" text-2xl font-bold">{nombre}</h3>
+            <p className=" mt-5 font-black text-4xl text-amber-500">{precio}</p>
+        </div>
+
     </div>
   )
 }
