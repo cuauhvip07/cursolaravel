891bedca1a56136ba125d4ad0734252ce84c9fc2
cuauhvip07 <cuauvillalba1@gmail.com>
2024-05-25 09:51:25 -0600

    Cantidad producto
    Se ajusta la cantidad de los productos y cuantos productos quiere el cliente

--- react-quiosco/src/layouts/components/ModalProducto.jsx
+++ react-quiosco/src/layouts/components/ModalProducto.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import useQuiosco from "../../hooks/useQuiosco"
 import { formatearDinero } from "../../helpers";
 
 export default function ModalProducto() {
 
     const {producto, handleClickModal } = useQuiosco();
+    const [cantidad, setCantidad] = useState(1);
   return (
 
     <div className="md:flex gap-10">
@@ -24,6 +26,34 @@ export default function ModalProducto() {
 
             <h1 className=" text-3xl font-bold mt-5">{producto.nombre}</h1>
             <p className=" mt-5 font-black text-5xl text-amber-500">{formatearDinero(producto.precio)}</p>
+
+            <div className=" flex gap-4 mt-5">
+                <button
+                    type="button"
+                    onClick={() => {
+                        if( cantidad >= 5) return
+                        setCantidad(cantidad + 1)
+                    }}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                </button>
+
+                <p className=" text-3xl">{cantidad}</p>
+
+                <button
+                    type="button"
+                    onClick={() => {
+                        if( cantidad <= 1) return
+                        setCantidad(cantidad - 1)
+                 }}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                </button>
+            </div>
             
             <button
                 type="button"
