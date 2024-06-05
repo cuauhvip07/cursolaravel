e5f804cba4fda08a24927d9532ff2a7a012a3792
cuauhvip07 <cuauvillalba1@gmail.com>
2024-06-04 20:50:17 -0600

    Creacion seeder
    Se hace un seed de las categorias y se suben a la tabla de la bd

--- laravel-quiosco/database/seeders/CategoriaSeeder.php
+++ laravel-quiosco/database/seeders/CategoriaSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class CategoriaSeeder extends Seeder
 {
@@ -12,6 +14,46 @@ class CategoriaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('categorias')->insert([
+            'nombre' => 'Café',
+            'icono' => 'cafe',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Hamburguesas',
+            'icono' => 'hamburguesa',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Pizzas',
+            'icono' => 'pizza',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Donas',
+            'icono' => 'dona',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Pasteles',
+            'icono' => 'pastel',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Galletas',
+            'icono' => 'galletas',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
     }
 }
