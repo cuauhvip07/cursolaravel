e2a1230dc9782f4c44bf7e30cfbf9b941ff4862b
cuauhvip07 <cuauvillalba1@gmail.com>
2024-06-06 11:43:40 -0600

    useSWR
    Se ve el uso de SWR para consumir la API

--- react-quiosco/src/views/Inicio.jsx
+++ react-quiosco/src/views/Inicio.jsx
@@ -1,12 +1,24 @@
 
-import { productos as data } from '../data/productos'
+import useSWR from 'swr';
 import Producto from '../layouts/components/Producto'
 import useQuiosco from '../hooks/useQuiosco'
+import clienteAxios from '../config/axios';
 
 export default function Inicio() {
   const { categoriaActual } = useQuiosco();
+
   // Ayuda a filtar los productos y despues pasarlos al state 
-  const productos = data.filter(producto => producto.categoria_id === categoriaActual.id)
+  // Consulta SWR
+  const fetcher = () => clienteAxios('/api/productos').then(data => data.data);
+  const { data, error, isLoading } = useSWR('/api/productos', fetcher,{
+    refreshInterval: 1000
+  })
+ 
+  // isLoading es para que aparezca mientras carga la pagina
+  if(isLoading) return 'Cargando...'
+  // Es data.data por que se trae un arreglo de objetos
+
+  const productos = data.data.filter(producto => producto.categoria_id === categoriaActual.id)
   return (
     <>
       <h1 className=' text-4xl font-black'>{categoriaActual.nombre}</h1> 
