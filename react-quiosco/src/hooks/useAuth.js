8ed27df37283b76108686fcb7a9b2ce17c8f0e25
cuauhvip07 <cuauvillalba1@gmail.com>
2024-07-17 23:54:22 -0600

    Revalidación del usuario
    Se revalida el usuario con mutate para estar seguros que es dicho usuario

--- react-quiosco/src/hooks/useAuth.js
+++ react-quiosco/src/hooks/useAuth.js
@@ -1,9 +1,29 @@
-
+import useSWR from "swr";
 import clienteAxios from "../config/axios";
 
 export const useAuth = (middleware, url) => {
 
+    const token = localStorage.getItem('AUTH_TOKEN')
+
+    // useSWR y el cliente axios deben de ser la misma url
+    const {data: user,error, mutate} = useSWR('/api/user', () =>
+        clienteAxios('/api/user', {
+            // Con axios se debe de pasar mediante headers
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        // Se debe de usar promises ya que no es asincrono
+        // El res da respuestas y en data esta la informacion del usuario
+        .then(res => res.data)
+        .catch(error => {
+            throw Error(error?.response?.data?.errors)
+        })
+    )
+
     const login = async (datos,setErrores) => {
+
+       
 
         try {
 
@@ -12,7 +32,8 @@ export const useAuth = (middleware, url) => {
             // Se almacena el token en localStorage
             localStorage.setItem('AUTH_TOKEN',data.token)
             setErrores([]);
-            
+            // mutate forza la revalidacion de los usuarios
+            await mutate()
         } catch (error) {
 
             if (error.response && error.response.data && error.response.data.errors) {
@@ -28,6 +49,9 @@ export const useAuth = (middleware, url) => {
     const logout = () => {
 
     }
+
+    console.log(user)
+    console.log(error)
 
     return {
         login,
