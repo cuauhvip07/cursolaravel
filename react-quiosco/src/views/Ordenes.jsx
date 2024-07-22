44198329985bf6a6ea42145aefdceb3692a9ebe1
cuauhvip07 <cuauvillalba1@gmail.com>
2024-07-22 11:03:22 -0600

    Finalizacion mostrar Ordenes
    Se termina el diseño para mostrar las ordenes y todas las ordenes se muestran en la pantalla del administrador

--- react-quiosco/src/views/Ordenes.jsx
+++ react-quiosco/src/views/Ordenes.jsx
@@ -12,7 +12,7 @@ export default function Ordenes() {
     }
   })
 
-  const {data,error,isLoading} = useSWR('/api/pedidos', fetcher)
+  const {data,error,isLoading} = useSWR('/api/pedidos', fetcher, {refreshInterval: 1000})
   // console.log(data)
   if(isLoading) return 'Cargando...'
 
@@ -21,7 +21,7 @@ export default function Ordenes() {
         <h1 className=' text-4xl font-black'>Ordenes</h1> 
         <p className=' text-2xl my-10'>Administra las ordenes desde aqui</p>
 
-        <div>
+        <div className=" md:grid md:grid-cols-2 gap-2">
           {data.data.data.map(pedido => (
             <div key={pedido.id} className="p-5 bg-white shadow space-y-2 border-b">
               <p className=" text-xl font-bold text-slate-600">Contenido del pedido:</p>
@@ -31,12 +31,29 @@ export default function Ordenes() {
                   <p className="text-sm">ID: {producto.id}</p>
                   <p className="text-sm">{producto.nombre}</p>
                   <p >Cantidad: {''}
-                    <span className=" font-bold">{formatearDinero(producto.pivot.cantidad)}</span>
+                    <span className=" font-bold">{producto.pivot.cantidad}</span>
                   </p>
                 </div>
               ))}
+
+              <p className=" text-lg font-bold text-slate-600">
+                Cliente: {''}
+                <span className="font-normal">{pedido.user.name}</span>
+              </p>
+
+              <p className="text-lg font-bold text-amber-500">
+                Total a pagar: {''}
+                <span className="font-normal text-slate-600">{formatearDinero(pedido.total)}</span>
+              </p>
+
+              <button 
+                type="button" 
+                className='bg-indigo-600  hover:bg-indigo-800 cursor-pointer px-5 py-2 rounded uppercase font-bold text-white text-center w-full'
+              >Completar</button>
             </div>
           ))}
+
+         
         </div>
     </div>
   )
